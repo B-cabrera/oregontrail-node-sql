5d544d7aae63413ced65baec05203916e4b9bf68
B-cabrera <brendenc29@gmail.com>
2022-10-05 20:20:34 -0400

    Phase 1: Set up folders/files and changed root

--- Project/Server/oregonTrail.js
+++ Project/Server/oregonTrail.js
@@ -2,8 +2,11 @@ const express = require('express' );
 const app = express();
 const port = 1337;
 
+
+app.use(express.static('Client/public'));
+
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.sendFile('index.html', {root: './Client/views'});
 });
 
 app.listen(port, () => {
