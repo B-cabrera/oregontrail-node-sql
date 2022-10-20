61a06ce43d183ea07f25a51625db5565f527df83
B-cabrera <brendenc29@gmail.com>
2022-10-20 00:56:16 -0400

    Fixed background and file paths

--- Project/Server/oregonTrail.js
+++ Project/Server/oregonTrail.js
@@ -3,35 +3,28 @@ const app = express();
 const port = 1337;
 
 
-app.use(express.static('Client/public'));
+app.use(express.static('../Client/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: './Client/views'});
+    res.sendFile('index.html', {root: '../Client/views'});
 });
 
-/* 
-NOTE !!! FROM HERE TO END HAS BEEN CODED ON A MACHINE
-WITHOUT NODE.JS. CODE WAS ONLY WRITTEN, NOT TESTED.
-PLEASE TEST AND REMOVE THIS MESSAGE WHEN TESTED
-*/
-// START
+
 app.get('/mainmenu', (req, res) => {
-    res.sendFile('mainmenu.html', {root: '.Client/views'});
+    res.sendFile('mainmenu.html', {root: '../Client/views'});
 });
 
 app.get('/topten', (req, res) => {
-    res.sendFile('topten.html', {root: '.Client/views'});
+    res.sendFile('topten.html', {root: '../Client/views'});
 });
 
 app.get('/setup', (req, res) => {
-    res.sendFile('setup.html', {root: '.Client/views'});
+    res.sendFile('setup.html', {root: '../Client/views'});
 });
 
 app.get('/trail', (req, res) => {
-    res.sendFile('trail.html', {root: '.Client/views'});
+    res.sendFile('trail.html', {root: '../Client/views'});
 });
-
-//END
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
