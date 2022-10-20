4eeef159d02c7a7196740cb85ad7173149138228
Brenden Cabrera <brendenc29@gmail.com>
2022-10-19 20:54:09 -0400

    Added and Setup Files

--- Project/Server/oregonTrail.js
+++ Project/Server/oregonTrail.js
@@ -9,6 +9,30 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', {root: './Client/views'});
 });
 
+/* 
+NOTE !!! FROM HERE TO END HAS BEEN CODED ON A MACHINE
+WITHOUT NODE.JS. CODE WAS ONLY WRITTEN, NOT TESTED.
+PLEASE TEST AND REMOVE THIS MESSAGE WHEN TESTED
+*/
+// START
+app.get('/mainmenu', (req, res) => {
+    res.sendFile('mainmenu.html', {root: '.Client/views'});
+});
+
+app.get('/topten', (req, res) => {
+    res.sendFile('topten.html', {root: '.Client/views'});
+});
+
+app.get('/setup', (req, res) => {
+    res.sendFile('setup.html', {root: '.Client/views'});
+});
+
+app.get('/trail', (req, res) => {
+    res.sendFile('trail.html', {root: '.Client/views'});
+});
+
+//END
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 }); 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cmpt221arias
+
+
+
+## Project 
+
+ ### To-Do
+ - [ ] Fix White Border on Background image in index.html
+
+ ### Progress
+
+I am on Step 4 - #1 Index.html
